f7997be09de28735bd8f92bf0f367826ce70b5d5
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-09-21 19:07:08 +0600

    new playing movie card design

--- src/pages/NowPlayingMovies/NowPlayingMoviesCard.jsx
+++ src/pages/NowPlayingMovies/NowPlayingMoviesCard.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 
-
-
 const NowPlayingMoviesCard = ({ item }) => {
     const { name, image, duration, category } = item
     return (
         <div>
-            <div className='rounded-xl shadow-lg p-6 bg-blue-100 text-center my-5 flex flex-col justify-center items-center'>
+            <div className='rounded-xl shadow-lg p-6 bg-blue-950 text-center my-5 flex flex-col justify-center items-center'>
                 <div className=' mx-auto'>
                     <img src={image} alt="movie" className="w-[250px] h-[250px] mb-4 rounded-lg" />
                 </div>
-                <div>
 
-                    <h2 className="">{name}</h2>
+                <div>
+                    <div className='text-fuchsia-600 font-bold'>{name}</div>
                     <div className='flex flex-row gap-1'>
-                        <p className='mb-4 text-pink-600 text-xl'>{category}</p>
-                        <p className='mb-4 text-pink-600 text-xl'> <span>/</span> {duration}</p>
+                        <p className='mb-4 text-fuchsia-600 text-xl'>{category}</p>
+                        <p className='mb-4 text-fuchsia-600 text-xl'> <span>/</span> {duration}</p>
+
                     </div>
+
 
                 </div>
             </div>

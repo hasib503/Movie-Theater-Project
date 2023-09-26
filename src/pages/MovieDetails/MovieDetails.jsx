f38608554ef1f4ab2e930dfb4ebe2c4eaccf07d8
nahid-dev <nahidv232@gmail.com>
2023-09-27 00:58:29 +0600

    movie details page rating done

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Container from "../../components/container/Container";
+import CircleRating from "../../components/circleRating/CircleRating";
 
 const data = {
   cover: "https://i.ibb.co/BsQrnNL/nun-2-cover.jpg",
@@ -8,24 +9,14 @@ const data = {
   storyline:
     "A follow-up to the enigmatic gothic horror about a strong evil that haunts and causes supernatural harm to everybody it comes into contact with. After the events of the first film, the said powerful evil now begins to spread in 1956 throughout a town in France as word gets out that a priest has been violently murdered. A finished contemplative in her novitiate, Sister Irene, begins to investigate the murder, only to find a demon behind it -- the same evil that terrorized her in the original film as a nun -- Valak, whom she once again soon comes to encounter.",
   genre: ["Horror", "Mystery", "Thriller"],
-  cast1: [],
-  cast2: [
-    "Jonas Bloquet",
-    "https://static.wikia.nocookie.net/1899-netflix/images/c/c8/Jonas_Bloquet.jpg/revision/latest?cb=20210506172715",
-  ],
-  cast3: [
-    "Storm Reid",
-    "https://facts.net/wp-content/uploads/2023/07/47-facts-about-storm-reid-1688396145.jpg",
-  ],
-  cast4: [
-    "Anna Popplewell",
-    "https://static.wikia.nocookie.net/narnia/images/7/79/200px-Susanpevensie.jpg/revision/latest?cb=20110909112859",
-  ],
   releaseDate: "8 September, 2023",
   runtime: "1 hour 50 minutes",
   category: ["2D", "3D"],
   language: "English",
   trailer: "https://www.youtube.com/watch?v=QF-oyCwaArU",
+  imDb_rating: 8.8,
+  Director: "Michael Chaves",
+  Writer: ["Ian Goldberg", "Richard Naing", "Akela Cooper"],
   casts: [
     {
       cast_name: "Jonas Bloquet",
@@ -52,23 +43,36 @@ const data = {
 const MovieDetails = () => {
   return (
     <div className="">
-      <div className="h-[500px] relative">
-        <img className="w-full object-cover h-full" src={data.cover} alt="" />
-        <div className="absolute  top-0 bg-[#001232] bg-opacity-60 w-full h-full">
+      <div className="h-[550px] relative">
+        <img className="w-full object-cover h-full" src={data?.cover} alt="" />
+        <div className="absolute  top-0 bg-[#001232] bg-opacity-80 w-full h-full">
           <Container>
-            <div className="h-full pt-8">
-              <div className="grid gird-cols-1 md:grid-cols-1 lg:grid-cols-2  backdrop-blur-sm rounded-xl">
-                <div className="">
-                  <div className=" w-[300px]">
+            <div className="h-[550px] flex items-center">
+              <div className="grid gird-cols-1 md:grid-cols-1 lg:grid-cols-3 w-full">
+                <div className="col-span-1 ">
+                  <div className="w-[350px] h-auto">
                     <img
-                      className="rounded-xl"
+                      className="shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px]"
                       src={data?.poster}
                       alt="Main Poster"
                     />
                   </div>
                 </div>
-                <div>
-                  <h2 className="text-3xl text-light">{data?.name}</h2>
+                <div className="col-span-2 text-white space-y-4">
+                  <h2 className="text-6xl ">{data?.name}</h2>
+                  <div className="flex items-center py-3 space-x-5">
+                    {data?.genre?.map((item, index) => (
+                      <span
+                        className="bg-[#d42a2a] px-1  text-sm rounded-sm"
+                        key={index}
+                      >
+                        {item}
+                      </span>
+                    ))}
+                  </div>
+                  <div>
+                    <CircleRating rating={data.imDb_rating}></CircleRating>
+                  </div>
                 </div>
               </div>
             </div>
@@ -77,7 +81,7 @@ const MovieDetails = () => {
       </div>
       {/* Cast section */}
       <Container>
-        <div className="backdrop-blur-lg">
+        <div className="backdrop-blur-lg hidden">
           {data?.casts?.map((cast, index) => (
             <p key={index}>{cast.cast_name}</p>
           ))}

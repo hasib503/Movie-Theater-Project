c1d5eccc55a607d3a16fd4e7c4a7deb311f4d69a
nahid-dev <nahidv232@gmail.com>
2024-01-07 01:03:10 +0600

    movie details page first part modified done

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -28,54 +28,57 @@ const MovieDetails = () => {
 
   return (
     <div className="">
-      <div className="h-[900px] lg:h-[550px] relative">
+      <div className="h-[900px] lg:h-[550px] relative ">
         <img
           className="w-full object-cover h-full"
           src={movieDetails?.cover}
           alt=""
         />
-        <div className="absolute  top-0 bg-[#001232] bg-opacity-80 w-full h-full">
+        <div className="absolute  top-0 bg-[#001232] bg-opacity-80 w-full h-full ">
           <Container>
             <div className="h-[550px] flex items-center">
-              <div className="grid gird-cols-1 md:grid-cols-1 lg:grid-cols-3 w-full h-full mt-10 lg:mt-0">
-                <div className="col-span-3 lg:col-span-1 ">
-                  <div className=" md:w-[400px]">
+              <div className="grid gird-cols-1 md:grid-cols-1 lg:grid-cols-3 w-full h-full mt-10 lg:mt-0 ">
+                <div className="col-span-3 lg:col-span-1">
+                  <div className="  h-full flex items-center justify-center">
                     <img
-                      className=" shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] "
+                      className=" shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] w-[300px]"
                       src={movieDetails?.poster}
                       alt="Main Poster"
                     />
                   </div>
                 </div>
                 {/* Details content */}
-                <div className="col-span-3 lg:col-span-2 text-white space-y-4 lg:ml-10 mt-10 md:mt-0 ">
-                  <h2 className="md:text-6xl text-4xl">{movieDetails?.name}</h2>
-                  <div className="flex items-center py-3 space-x-5">
-                    {movieDetails?.genre?.map((item, index) => (
-                      <span
-                        className="bg-[#d42a2a] px-1  text-sm rounded-sm"
-                        key={index}
-                      >
-                        {item}
-                      </span>
-                    ))}
-                  </div>
-                  <div className="flex items-center gap-5">
-                    <CircleRating
-                      rating={movieDetails?.imdb_rating}
-                    ></CircleRating>
-                    <div
-                      className="playbtn"
-                      onClick={() => {
-                        setShow(true);
-                      }}
-                    >
-                      <PlayIcon></PlayIcon>
-                      <span className="text">Watch Trailer</span>
+                <div className=" col-span-3 lg:col-span-2 text-white space-y-4 lg:pl-10 mt-10 md:mt-0 flex items-center ">
+                  <div className="w-full">
+                    <h2 className="md:text-6xl text-4xl">
+                      {movieDetails?.name}
+                    </h2>
+                    <div className="flex items-center py-3 space-x-5">
+                      {movieDetails?.genre?.map((item, index) => (
+                        <span
+                          className="bg-[#d42a2a] px-1  text-sm rounded-sm"
+                          key={index}
+                        >
+                          {item}
+                        </span>
+                      ))}
                     </div>
-                  </div>
-                  <div className="Other_info">
-                    {/* <table className="table">
+                    <div className="flex items-center gap-5">
+                      <CircleRating
+                        rating={movieDetails?.imdb_rating}
+                      ></CircleRating>
+                      <div
+                        className="playbtn"
+                        onClick={() => {
+                          setShow(true);
+                        }}
+                      >
+                        <PlayIcon></PlayIcon>
+                        <span className="text">Watch Trailer</span>
+                      </div>
+                    </div>
+                    <div className="Other_info">
+                      {/* <table className="table">
                       <tbody>
                         <tr className="border-b border-[#2f3f79] md:text-lg">
                           <td className="w-[30%] pl-0 ">Language :</td>
@@ -111,6 +114,7 @@ const MovieDetails = () => {
                         </tr>
                       </tbody>
                     </table> */}
+                    </div>
                   </div>
                 </div>
               </div>

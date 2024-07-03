c13d301ac9340359c17d7bb02c205ff2f8f452d6
Hasib24 <hasibislam1488@gmail.com>
2024-07-03 15:49:14 +0600

    initialize basic ticket system

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children }) => {
   return (
-    <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
+    <div className="max-w-[1356px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
       {children}
     </div>
   );
--- src/pages/ticketPrice/TicketPrice.jsx
+++ src/pages/ticketPrice/TicketPrice.jsx
@@ -1,10 +1,89 @@
 import React from "react";
-import DummyCompo from "../../components/dummyCompo/DummyCompo";
+import Container from "../../components/container/Container";
+import { PiArmchairLight } from "react-icons/pi";
 
 const TicketPrice = () => {
+
+  let movieTickets = [
+    {
+      "id": 1,
+      "status": "empty",
+      "movie": "Example Movie",
+      "time": "19:00",
+      "date": "2024-07-05",
+      "seat": "A1",
+      "price": 12.5
+    },
+    {
+      "id": 2,
+      "status": "empty",
+      "movie": "Example Movie",
+      "time": "19:00",
+      "date": "2024-07-05",
+      "seat": "A2",
+      "price": 12.5
+    },
+    {
+      "id": 3,
+      "status": "empty",
+      "movie": "Example Movie",
+      "time": "19:00",
+      "date": "2024-07-05",
+      "seat": "A3",
+      "price": 12.5
+    },
+    {
+      "id": 4,
+      "status": "booked",
+      "movie": "Example Movie",
+      "time": "19:00",
+      "date": "2024-07-05",
+      "seat": "A4",
+      "price": 12.5
+    },
+    {
+      "id": 5,
+      "status": "empty",
+      "movie": "Example Movie",
+      "time": "19:00",
+      "date": "2024-07-05",
+      "seat": "A5",
+      "price": 12.5
+    }
+  ]
+
+
+  const handleClickOnTicket = (ticket) => {
+    if (ticket.status = "booked") {
+      console.log(`Sorry, ticket numbere ${ticket.seat} is already booked`);
+    } else {
+      console.log(`Ticket number ${ticket.seat} is available for booking !`);
+    }
+
+  }
+
   return (
-    <div>
-      <DummyCompo></DummyCompo>
+    <div className="min-h-[50vh]">
+      <Container>
+        <div className="mx-4 flex">
+          {
+            movieTickets.map((ticket) => {
+              return (
+                <div onClick={() => handleClickOnTicket(ticket)} key={ticket.id}
+                  className={ticket.status == "booked" ? `m-2 p-1 border rounded-md border-yellow-600 bg-yellow-500 text-slate-900 cursor-not-allowed` : `m-2 p-1 border rounded-md border-sky-100 cursor-pointer`}
+                ><PiArmchairLight className="h-6 w-6" />
+                </div>
+
+              )
+            })
+
+
+          }
+
+
+        </div>
+
+      </Container>
     </div>
   );
 };
